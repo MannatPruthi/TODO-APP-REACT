31351eaac3969398d8481e3c8289ce5873a084d9
Mannat Pruthi <91721574+MannatPruthi@users.noreply.github.com>
2024-02-20 18:56:46 +0530

    Add files via upload

---
+++ src/components/TodoItem1.jsx
@@ -0,0 +1,20 @@
+function TodoItem1() {
+  let todoName = 'Buy Milk';
+  let todoDate = '18/02/2024';
+
+  return (
+    <div className="container">
+      <div className="row man-row">
+        <div className="col-6">{todoName}</div>
+        <div className="col-4">{todoDate}</div>
+        <div className="col-2">
+          <button type="button man-button" className="btn btn-danger">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem1;
